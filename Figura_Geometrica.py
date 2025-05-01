e3893c218e16c1750449ffeb139fd70c898ec6d8
user <ug-fca-gig@ug.gob.ec>
2025-05-01 11:53:39 -0500

    Figura Geométrica

---
+++ Figura_Geometrica.py
@@ -0,0 +1,19 @@
+class figura_geometrca
+    def __init__(self, alto= 0, ancho= 0):
+        self._alto = alto
+        self._ancho = ancho
+    def get_ancho(self):
+        return self._ancho
+    def get_alto(self):
+        return self._alto
+    def get_ancho(self,ancho):
+        self.ancho = ancho
+    def get_alto(self, alto):
+        self.alto = alto
+def str_(self):
+    return f'alto: {self.alto}, ancho: {self.ancho}´
+
+class cuadrado(figura_geometrca):
+    def __init__(self, alto= 0, ancho= 0):
+        super().__init__(alto, ancho)
+        self.
